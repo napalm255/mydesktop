da27f9d623dfe8c6cd44949a1005e4ed1e7ba3b4
Brad Gibson <bgibson@promnetwork.com>
2014-02-14 23:45:18 -0500

    misc updates

--- mydesktop/manifests/repos.pp
+++ mydesktop/manifests/repos.pp
@@ -91,8 +91,8 @@ class mydesktop::repos {
   # setup tmp directory
   file { '/tmp/wemux/' :
     ensure => directory,
-    owner  => $user,
-    group  => $user,
+    owner  => $mydesktop::user,
+    group  => $mydesktop::user,
     mode   => '1777',
   }
 }
