22e7486102bc36392407e45f070f769982c03d56
Brad Gibson <bgibson@promnetwork.com>
2014-02-17 21:50:19 -0500

    misc added vim colorschemes

--- mydesktop/manifests/vim.pp
+++ mydesktop/manifests/vim.pp
@@ -66,4 +66,10 @@ class mydesktop::vim {
     user     => $mydesktop::user,
   }
 
+  vcsrepo { "/home/${mydesktop::user}/.vim/bundle/colorschemes" :
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/flazz/vim-colorschemes.git',
+    user     => $mydesktop::user,
+  }
 }
