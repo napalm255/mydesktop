e9d08066ac01e9a199f67c3642503bc53e5f1b7e
napalm255 <napalm255@gmail.com>
2014-02-23 12:20:18 -0500

    added p7zip

--- mydesktop/manifests/system.pp
+++ mydesktop/manifests/system.pp
@@ -1,6 +1,6 @@
 class mydesktop::system () {
 
-  $tools_cli = [ 'git', 'puppet', 'htop', 'nmap', 'lynx', 'wget', 'whois', 'ipcalculator', 'figlet', 'telnet', 'traceroute', 'samba-client', 'samba-common', 'autofs', 'putty', 'tmux', 'vim-enhanced', 'python-pip', 'iperf3' ]
+  $tools_cli = [ 'git', 'puppet', 'htop', 'nmap', 'lynx', 'wget', 'whois', 'ipcalculator', 'figlet', 'telnet', 'traceroute', 'samba-client', 'samba-common', 'autofs', 'putty', 'tmux', 'vim-enhanced', 'python-pip', 'iperf3', 'p7zip', 'p7zip-plugins' ]
   $tools_gui = [ 'xpad', 'gnome-tweak-tool', 'gimp', 'geany', 'bluefish' ]
   $tools_ssh = [ 'openssh', 'openssh-server', 'openssh-clients' ]
   $tools_dev = [ 'kernel-headers', 'kernel-devel', 'gcc', 'binutils', 'make', 'patch', 'libgomp', 'glibc-headers', 'glibc-devel', 'dkms' ]
